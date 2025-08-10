b37c914882ff20345599af6e6f2f9fab92a4148b
yunshin <2younsin@gmail.com>
2025-08-10 18:18:10 +0900

    INIT: (none) 아이콘별 개별 진입점 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   plugins: [
@@ -12,10 +14,14 @@ export default defineConfig({
       entryRoot: 'src',
       //생성된 .d.ts 파일들이 어디에 저장될지 지정
       outDir: 'dist',
-      // 타입 정의 진입점(index.d.ts)을 자동 생성, package.json의 "types": "dist/index.d.ts"와 연결됨
-      insertTypesEntry: true,
+      // Storybook 파일 제외 (.stories.ts / .stories.tsx)
+      exclude: ['**/*.stories.ts', '**/*.stories.tsx', '**/*.stories.js', '**/*.stories.jsx'],
     }),
+    tsconfigPaths(),
   ],
+  resolve: {
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
+  },
   css: {
     modules: {
       // SSR 시 클래스명이 서버/클라이언트 동일하도록 결정적 네이밍
@@ -26,20 +32,26 @@ export default defineConfig({
   },
   build: {
     lib: {
-      // Vite가 이 파일부터 코드를 분석해 필요한 모듈을 모두 번들링
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: {
+        'ui/BackBtn/index': resolve(__dirname, 'src/ui/BackBtn/index.tsx'),
+      },
       // 빌드할 모듈 형식을 지정. (ES Modules 만)
       formats: ['es'],
-      // 빌드 후 생성될 파일 이름 지정
-      fileName: () => 'index.js',
     },
     // 하나의 CSS로 합치기
     cssCodeSplit: false,
     // outDir 폴더를 빌드 전에 비워주기
     emptyOutDir: true,
+
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
+
       output: {
+        entryFileNames: '[name].js',
+        // 폴더 구조 유지
+        preserveModules: true,
+        // dist/ 안에 src 구조 그대로
+        preserveModulesRoot: 'src',
         // Vite가 보통 'style.css'로 내보내므로 'index.css'로 강제
         assetFileNames: (assetInfo) => {
           if (assetInfo.name?.endsWith('.css')) return 'index.css';
