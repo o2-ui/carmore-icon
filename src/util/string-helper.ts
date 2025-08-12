815272348bfa9624750e4166205ec3a7de2b2497
yunshin <2younsin@gmail.com>
2025-08-13 00:18:29 +0900

    REFACTOR: (none) getElementId 결과값의 고유성 개선

--- src/util/string-helper.ts
+++ src/util/string-helper.ts
@@ -3,9 +3,18 @@ const mergeClassNames = (...classes: (string | undefined | false)[]) => {
 };
 
 const getElementId = (original?: string) => {
-  const originalId = original || String(Math.floor(Math.random() * 100000)).padStart(5, '0');
   const prefix = 'o2-ui-react-icon';
-  return [prefix, originalId].join('-');
+
+  let uniqueId = '';
+  if (typeof window !== 'undefined') {
+    uniqueId = window.crypto.randomUUID();
+  } else {
+    uniqueId = Array.from({ length: 5 })
+      .map(() => Math.random().toString(36).substring(2, 9))
+      .join('-');
+  }
+
+  return [prefix, uniqueId, original].filter(Boolean).join('-');
 };
 
 export { mergeClassNames, getElementId };
