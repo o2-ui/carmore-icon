579cca7e3a4b4ccd5560d36878a98b9140231b95
yunshin <2younsin@gmail.com>
2025-10-16 20:31:30 +0900

    FEAT: (v1.3.0) Pet 추가

---
+++ src/ui/Pet/Pet.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import Pet from '.';
+
+const meta: Meta<typeof Pet> = {
+  title: 'ui/Pet',
+  component: Pet,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    width: {
+      control: 'number',
+    },
+    height: {
+      control: 'number',
+    },
+    state: {
+      control: 'select',
+      options: ['on', 'off'],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Pet>;
+
+export const Preview: Story = {};
